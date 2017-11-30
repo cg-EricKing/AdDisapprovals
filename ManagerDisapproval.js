c17d3e6b1375a64ac0c48e26e72a9faf089eebf7
Eric King <cg100146@comporium.com>
2017-11-30 14:55:57 -0500

    Removed broken script - Added ad disapproval function to 50+ script - working

---
+++ ManagerDisapproval.js
@@ -0,0 +1,65 @@
+var SCRIPT_LABEL = 'CMS-IYP';
+
+function run() {
+  // your script goes here
+  var currentAccount = AdWordsAp.currentAccount();
+  var currentAccountName = currentAccount.getName();
+  Logger.log("Looking at this account: " + currentAccountName);
+      var adSelector = AdWordsAp
+          .ads()
+          .withCondition("Status = ENABLED")
+          .withCondition("ApprovalStatus = DISAPPROVED");
+  
+      var adIterator = adSelector.get();
+      // This would food the Logs window
+      // Logger.log("Total ads found: " + adIterator.totalNumEntities());
+  
+      // Loop over and log out the information for disapproved ads
+      while(adIterator.hasNext()) {
+          var ads = adIterator.next();
+          Logger.log("Account Flagged: " + currentAccountName);
+          Logger.log("Ad Status: " + ads.getApprovalStatus());
+          Logger.log("Ad Disapproval Reasons: " + ads.getDisapprovalReasons());
+          Logger.log("Ad Type: " + ads.getType());
+          // Remove the ads
+          // ad.remove();
+          Logger.log("================================================");
+      };
+}
+
+// this will execute your script sequentially accounts and is only used for accounts in excess of 50
+function executeInSequence(sequentialIds, executeSequentiallyFunc) {
+    sequentialIds.forEach(function (accountId) {
+        var account = MccApp.accounts().withIds([accountId]).get().next();
+        MccApp.select(account);
+        executeSequentiallyFunc();
+    });
+}
+
+// our custom main function responsible for executing the run function
+function main() {
+    try {
+        var accountSelector = MccApp.accounts().orderBy('Name');
+        if (SCRIPT_LABEL) {
+            accountSelector = accountSelector.withCondition("LabelNames CONTAINS '" + SCRIPT_LABEL + "'");
+        }
+        var accountIterator = accountSelector.get();
+        var accountIds = [];
+        while (accountIterator.hasNext()) {
+            var account = accountIterator.next();
+            accountIds.push(account.getCustomerId());
+        }
+        var parallelIds = accountIds.slice(0, 50);
+        var sequentialIds = accountIds.slice(50);
+        MccApp.accounts()
+            .withIds(parallelIds)
+            .executeInParallel('run');
+        if (sequentialIds.length > 0) {
+            executeInSequence(sequentialIds, run);
+        }
+    }
+    catch (exception) {
+        Logger.log('Running on non-MCC account.');
+        run();
+    }
+}
